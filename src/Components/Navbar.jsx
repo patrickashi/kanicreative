c41e80e1eec4ec33102fb2f33ac0c7949198ec47
patrickashi <patrickpeko1@gmail.com>
2024-01-12 13:07:19 +0100

    new
    nn

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,7 +5,7 @@ import logo1 from "../assets/logo1.jpg"
 import { BsPerson } from 'react-icons/bs';
 import { BiSearch } from 'react-icons/bi';
 import { AiOutlineClose } from 'react-icons/ai';
-import { HiOutlineMenuAlt4 } from 'react-icons/hi';
+import { HiOutlineMenu } from 'react-icons/hi';
 import {
   FaFacebook,
   FaTwitter,
@@ -29,7 +29,7 @@ const Navbar = () => {
         <img className='text-white bg-white w-20 md:ml-3 ' size={20} src={logo1} alt="/" />
       </div>
       <ul className='hidden mobile:hidden 2xl:flex xl:flex lg:flex md:flex gap-4'>
-        <Link to="/"> Home </Link>
+        <Link to="/" > Home </Link>
         <Link to=""> About</Link>
         <Link to="/"> Contact </Link>
         <Link to="/"> Services </Link>
@@ -41,7 +41,7 @@ const Navbar = () => {
         <button className='border rounded-md px-4 py-1 border-[#059669] mr-6'>Login</button>
         {/* Hamburger */}
         <div onClick={handleNav} className='md:hidden z-10 cursor-pointer'>
-        {nav ? <AiOutlineClose className='text-white' size={20} /> : <HiOutlineMenuAlt4 size={20} />}
+        {nav ? <AiOutlineClose className='text-white' size={20} /> : <HiOutlineMenu size={20} />}
       </div>
 
       </div>
@@ -52,7 +52,7 @@ const Navbar = () => {
 
       
       {/* Mobile menu dropdown */}
-      <div onClick={handleNav} className={nav ? 'absolute text-white h-screen  left-0 top-0 w-full bg-[#020617] px-4 py-7 flex flex-col' : 'absolute left-[-100%]'}>
+      <div onClick={handleNav} className={nav ? 'absolute text-white h-screen  left-0 top-0 w-full bg-black px-4 py-7 flex flex-col' : 'absolute left-[-100%]'}>
         <ul>
           <div>
             <img src={logo1} size={20} className='w-20' alt="image" />
@@ -65,16 +65,15 @@ const Navbar = () => {
             <Link to="/"> Faq</Link>
             <NavLink to="/Dashhome"> Dashboard</NavLink>
           </div>
-          <div className='flex flex-col'>
-            <button className='my-6'>Search</button>
-            <button>Account</button>
+          <div className='flex flex-col '>
+            <button className='my-3 rounded-lg border border-[#059669] py-2'>Sign up</button>
+            <button className='rounded-lg border border-[#059669] py-2'>Account</button>
           </div>
-          <div className='flex justify-between my-6'>
-            <FaFacebook className='icon' />
-            <FaTwitter className='icon' />
-            <FaYoutube className='icon' />
-            <FaPinterest className='icon' />
-            <FaInstagram className='icon' />
+          <div className='flex justify-start gap-6 my-6 '>
+            <FaFacebook className='icon text-[#059669]' />
+            <FaTwitter className='icon text-[#059669]' />
+            <FaYoutube className='icon text-[#059669]' />
+            <FaInstagram className='icon text-[#059669]' />
           </div>
         </ul>
       </div>
