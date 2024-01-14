02164c2fb6aed78741dfe4b07a73101ae00a5495
patrickashi <patrickpeko1@gmail.com>
2024-01-14 22:42:51 +0100

    new
    add new

--- src/Components/Secthree.jsx
+++ src/Components/Secthree.jsx
@@ -18,12 +18,12 @@ const Secthree = () => {
                     <div className=" bg-gray-200 text-black py-8 px-4  rounded-lg px-16 sm:mx-10 md:mx-10 lg:mx-10 xl:mx-10 2xl:mx-10">
                         {/* card */}
                         <p>
-                            <FaQuoteLeft className="text-gray-600" size={30}/>
+                            <FaQuoteLeft className="text-gray-600 mb-2" size={30}/>
                         </p>
                         <p className="">
-                            The speed in which they came up a concept for me with the 
-                            limited information that i gave them really exceeded my expectations
-                            and great value
+                            I highly recommend Kani Creative for anyone seeking a top-notch web design experience.
+                            They go above and beyond to bring your ideas to life, and the results speak for themselves.
+                            
                         </p>
                         <div className="flex py-6 md:justify-center">
                             <img className="w-14 h-[53px] object-cover rounded-full " src={profile2} alt="" />
@@ -43,7 +43,7 @@ const Secthree = () => {
                     <div className=" bg-gray-200 text-black py-8 px-4  rounded-lg px-16 sm:mx-10 md:mx-10 lg:mx-10 xl:mx-10 2xl:mx-10">
                         {/* card */}
                         <p>
-                            <FaQuoteLeft className="text-gray-600" size={30}/>
+                            <FaQuoteLeft className="text-gray-600 mb-2" size={30}/>
                         </p>
                         <p className="">
                             The speed in which they came up a concept for me with the 
